193884004d99259f8d3dda82a251cab9567b576e
yurishe0 <yurishe00@gmail.com>
2024-12-04 18:18:21 +0100

    feat: implement registering users to db

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,24 @@
+const bcrypt = require('bcryptjs');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const indexController = {
     getIndex: (req, res) => {
-        res.render('pages/index')
+        res.render('pages/index');
+    },
+    getRegister: (req, res) => {
+        res.render('pages/register')
+    },
+    createUser: async (req, res) => {
+        req.body.password = await bcrypt.hash(req.body.password, 10)
+        const user = req.body;
+        await prisma.user.create({
+            data: {
+                username: user.username,
+                password: user.password
+            }
+        })
+        res.redirect('/');
     }
 }
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,5 +3,7 @@ const indexRouter = Router();
 const indexController = require('../controllers/indexController.js')
 
 indexRouter.get('/', indexController.getIndex);
+indexRouter.get('/register', indexController.getRegister);
+indexRouter.post('/register', indexController.createUser);
 
 module.exports = indexRouter;
