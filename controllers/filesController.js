3eaf802d13f3ec3e2e0c61163d8f15b27460a5d0
yurishe0 <yurishe00@gmail.com>
2024-12-05 17:27:25 +0100

    feat: implement file uploading via multer

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,6 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 const filesController = {
    getFiles: (req, res) => {
         res.render('pages/files')
--- routes/filesRouter.js
+++ routes/filesRouter.js
@@ -3,8 +3,11 @@ const filesRouter = Router();
 const filesController = require('../controllers/filesController.js')
 const isAuth = require('../middleware/isAuth.js');
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 filesRouter.get('/', isAuth, filesController.getFiles);
 filesRouter.get('/upload', isAuth, filesController.getUpload);
-filesRouter.post('/upload', isAuth, filesController.uploadFile);
+filesRouter.post('/upload', isAuth, upload.single('file'), filesController.uploadFile);
 
 module.exports = filesRouter;
