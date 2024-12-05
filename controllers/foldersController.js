d0c2913d4a584f4dcd35cf402607bad086fb7001
yurishe0 <yurishe00@gmail.com>
2024-12-05 18:01:24 +0100

    feat: functionality for adding folders

--- controllers/foldersController.js
+++ controllers/foldersController.js
@@ -7,6 +7,19 @@ const foldersController = {
             where: { userId: req.user.id }
         })
         res.render('pages/folders', { folders: folders });
+    },
+    getCreateFolder: (req, res) => {
+        res.render('pages/createFolder');
+    },
+    createFolder: async (req, res) => {
+        const folder = await prisma.folder.create({
+            data: {
+                name: req.body.name,
+                userId: req.user.id
+            }
+        })
+        console.log(folder);
+        res.redirect('/folders');
     }
 }
 
--- routes/foldersRouter.js
+++ routes/foldersRouter.js
@@ -3,5 +3,7 @@ const foldersRouter = Router();
 const foldersController = require('../controllers/foldersController.js')
 
 foldersRouter.get('/', foldersController.getFolders);
+foldersRouter.get('/create', foldersController.getCreateFolder);
+foldersRouter.post('/create', foldersController.createFolder);
 
 module.exports = foldersRouter;
