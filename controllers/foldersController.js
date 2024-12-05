0849f8335c438f57c45dd5c53745af2f10039cff
yurishe0 <yurishe00@gmail.com>
2024-12-05 18:02:16 +0100

    fix: await for db operation

--- controllers/foldersController.js
+++ controllers/foldersController.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 
 const foldersController = {
     getFolders: async (req, res) => {
-        const folders = prisma.folder.findMany({
+        const folders = await prisma.folder.findMany({
             where: { userId: req.user.id }
         })
         res.render('pages/folders', { folders: folders });
